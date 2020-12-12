2d0f98eedf4012dd26b6c5cac53a38c3b3265ef8
Thai Nguyen <thainguyen.uet@gmail.com>
2020-12-12 21:57:44 +0700

    Temporarily added menu screen

--- core/src/uet/thainguyen/game/BommanGame.java
+++ core/src/uet/thainguyen/game/BommanGame.java
@@ -6,15 +6,17 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import uet.thainguyen.game.screens.MenuScreen;
 import uet.thainguyen.game.screens.PlayScreen;
 
 public class BommanGame extends Game {
-	SpriteBatch spriteBatch;
+
+	public SpriteBatch spriteBatch;
 	
 	@Override
 	public void create () {
 		spriteBatch = new SpriteBatch();
-		setScreen(new PlayScreen());
+		setScreen(new MenuScreen(this));
 	}
 
 	@Override
--- desktop/src/uet/thainguyen/game/desktop/DesktopLauncher.java
+++ desktop/src/uet/thainguyen/game/desktop/DesktopLauncher.java
@@ -1,12 +1,18 @@
 package uet.thainguyen.game.desktop;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import uet.thainguyen.game.BommanGame;
 
+import java.io.File;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.addIcon("ui/img/icon_01.jpg", Files.FileType.Internal);
+		config.title = "Super Bomman";
 		config.width = 544;
 		config.height = 480;
 		new LwjglApplication(new BommanGame(), config);
